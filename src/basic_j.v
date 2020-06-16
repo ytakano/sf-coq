7939280f31f82254c893d99392219faca0077cb0
Yuuki Takano <ytakanoster@gmail.com>
2020-06-17 06:18:50 +0900

    proofed basic_j.v

--- src/basic_j.v
+++ src/basic_j.v
@@ -761,14 +761,14 @@ Proof.
       reflexivity. } }
 Qed.
 
-Fixpoint nat_to_bin (n:nat) (b:bin) : bin :=
+Fixpoint nat_to_bin (n:nat) : bin :=
   match n with
-  | O => b
-  | S n' => nat_to_bin n' (bin_increment b)
+  | O => B0
+  | S n' => bin_increment (nat_to_bin n')
   end.
 
-Theorem nat_to_bin_1 : forall (n:nat),
-    nat_to_bin n (Bx1 B0) = nat_to_bin (S n) B0.
+Theorem nat_to_bin_comm : forall n:nat,
+    nat_to_bin (S n) = bin_increment (nat_to_bin n).
 Proof.
   intros n.
   induction n.
@@ -778,25 +778,14 @@ Proof.
     reflexivity. }
 Qed.
 
-Theorem nat_to_bin_comm : forall n:nat,
-    nat_to_bin n (Bx1 B0) = bin_increment (nat_to_bin n B0).
-Proof.
-  intros n.
-  induction n.
-  { simpl.
-    reflexivity. }
-  { simpl.
-Admitted.
-
-
 Theorem nat_bin_nat : forall n:nat,
-    bin_to_nat (nat_to_bin n B0) = n.
+    bin_to_nat (nat_to_bin n) = n.
 Proof.
   intros n.
   induction n.
   { simpl.
     reflexivity. }
-  { simpl.
+  { 
     rewrite -> nat_to_bin_comm.
     rewrite -> bin_to_nat_comm.
     rewrite -> IHn.
