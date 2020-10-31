6c62c3f7f0e4c2b078c32c1334149fb01a70080b
Yuuki Takano <ytakanoster@gmail.com>
2020-11-01 01:04:34 +0900

    finished Perm.v

--- src/Perm.v
+++ src/Perm.v
@@ -5,6 +5,9 @@ From Coq Require Import Arith.EqNat.
 From Coq Require Import omega.Omega.
 From Coq Require Import Lists.List.
 From Coq Require Import Strings.String.
+Require Export Permutation.
+Export ListNotations.
+
 
 Check lt.
 
@@ -108,4 +111,124 @@ Proof.
       bdestruct (a >? b).
       { omega. }
       { reflexivity. } } }
+Qed.
+
+Print Permutation.
+
+Definition manual_grade_for_Permutation_properties : option (nat * string) := None.
+
+Example butterfly : forall b u t e r f l y : nat,
+    Permutation ([b;u;t;t;e;r]++[f;l;y]) ([f;l;u;t;t;e;r]++[b;y]).
+Proof.
+  intros.
+  change [b;u;t;t;e;r] with ([b]++[u;t;t;e;r]).
+  change [f;l;u;t;t;e;r] with ([f;l]++[u;t;t;e;r]).
+  remember [u;t;t;e;r] as utter.
+  clear Hequtter.
+  change [f;l;y] with ([f;l]++[y]).
+  remember [f;l] as fl.
+  clear Heqfl.
+  replace ((fl++utter)++[b;y]) with (fl++utter++[b;y]) by apply app_assoc.
+  apply perm_trans with (fl++[y]++([b]++utter)).
+  { replace (fl++[y]++[b]++utter) with ((fl++[y])++[b]++utter).
+    { apply Permutation_app_comm. }
+    { rewrite <- app_assoc.
+      reflexivity. } }
+  { apply Permutation_app_head.
+    apply perm_trans with (utter++([y]++[b])).
+    { replace ([y]++[b]++utter) with (([y]++[b])++utter).
+      { apply Permutation_app_comm. }
+      { rewrite app_assoc.
+        reflexivity. } }
+    { apply Permutation_app_head.
+      apply perm_swap. } }
+Qed.
+
+Check perm_skip.
+Check perm_trans.
+Check Permutation_refl.
+Check Permutation_app_comm.
+Check app_assoc.
+Check app_nil_r.
+Check app_comm_cons.
+
+Example permut_example : forall (a b : list nat),
+    Permutation (5 :: 6 :: a ++ b) ((5 :: b) ++ (6 :: a ++ [])).
+Proof.
+  intros.
+  rewrite app_nil_r.
+  rewrite <- app_comm_cons.
+  apply perm_skip.
+  rewrite app_comm_cons.
+  apply Permutation_app_comm.
+Qed.
+
+Check Permutation_cons_inv.
+Check Permutation_length_1_inv.
+
+Example not_a_permutation:
+  ~ Permutation [1;1] [1;2].
+Proof.
+  unfold not.
+  intros.
+  apply Permutation_cons_inv in H.
+  apply Permutation_length_1_inv in H.
+  discriminate.
+Qed.
+
+Theorem maybe_swap_perm: forall al,
+    Permutation al (maybe_swap al).
+Proof.
+  unfold maybe_swap.
+  destruct al as [ | a [ | b al]].
+  { apply perm_nil. }
+  { apply Permutation_refl. }
+  { bdestruct (b <? a).
+    { apply perm_swap. }
+    { apply Permutation_refl. } }
+Qed.
+
+Definition first_le_second (al : list nat) : Prop :=
+  match al with
+  | a :: b :: _ => a <= b
+  | _ => True
+  end.
+
+Theorem maybe_swap_correct : forall al,
+    Permutation al (maybe_swap al) /\ first_le_second (maybe_swap al).
+Proof.
+  intros.
+  split.
+  { apply maybe_swap_perm. }
+  { unfold maybe_swap.
+    destruct al as [ | a [ | b al]]; simpl; auto.
+    bdestruct (a >? b); simpl; omega. }
+Qed.
+
+Ltac inv H := inversion H; clear H; subst.
+
+Print perm_nil.
+
+Print Forall.
+
+Print Permutation.
+
+Theorem Forall_perm: forall {A} (f : A -> Prop) al bl,
+    Permutation al bl -> Forall f al -> Forall f bl.
+Proof.
+  intros.
+  induction H.
+  { constructor. }
+  { constructor.
+    { inv H0.
+      assumption. }
+    { apply IHPermutation.
+      inv H0.
+      assumption. } }
+  { inv H0.
+    inv H3.
+    repeat constructor; assumption. }
+  { apply IHPermutation2.
+    apply IHPermutation1.
+    assumption. }
 Qed.
