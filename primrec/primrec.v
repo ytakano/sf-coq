b24403189f816840b3f8042c79108771bc2a8621
Yuuki Takano <ytakanoster@gmail.com>
2021-05-28 11:54:42 +0900

    added basic functions of primitive recursive function

--- primrec/primrec.v
+++ primrec/primrec.v
@@ -1,8 +1,11 @@
 Require Import List.
 Import ListNotations.
 
-Definition succ (n:nat) : nat :=
-  S n.
+Definition succ (n:list nat) : nat :=
+  match n with
+  | n'::_ => S n'
+  | _ => O
+  end.
 
 Definition proj (n:nat) (l:list nat) : nat :=
   nth n l O.
@@ -43,3 +46,29 @@ Definition recurse
   | h::t => recurse' f g h t
   | [] => O
   end.
+
+Definition plus (l:list nat): nat :=
+  match l with
+  | a::b::[] =>
+    let f := proj 0 in
+    let g := compose succ [proj 1] in
+    recurse f g [a; b]
+  | _ => 0
+  end.
+
+Compute plus [2; 4].
+
+Definition T: nat := O.
+Definition F: nat := S O.
+
+Definition If (l:list nat): nat :=
+  match l with
+  | cond::e1::e2::[] =>
+    match cond with
+    | O => e1
+    | _ => e2
+    end
+  | _ => O
+  end.
+
+Compute If [T; 20; 40].
